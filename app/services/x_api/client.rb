875df6c535f15f3c7db0fa451fb6ed5b41657881
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-20 15:01:49 +0900

    モックデータの追加

--- app/services/x_api/client.rb
+++ app/services/x_api/client.rb
@@ -9,11 +9,13 @@ module XApi
 
       # Gemが理解できる形式に整えて返す
       {
-        'screen_name' => raw_data['screen_name'],
+        'screen_name'     => raw_data['screen_name'],
         'followers_count' => raw_data['followers_count'],
         'following_count' => raw_data['following_count'],
-        'description' => raw_data['description'],
-        'created_at' => raw_data['created_at'] || "2026-01-01"
+        'description'     => raw_data['description'],
+        'created_at'      => raw_data['created_at'] || "2026-01-01",
+        'default_profile' => raw_data['default_profile'] || false, # ✨追加
+        'statuses_count'  => raw_data['statuses_count'] || 0        # ✨追加
       }
     end
   end
