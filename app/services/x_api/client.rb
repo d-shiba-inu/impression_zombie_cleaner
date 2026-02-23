d1c5efe96b77f8de8969747cc0d4310bba2d1bf3
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-23 16:20:52 +0900

    xAPIデータの変換

--- app/services/x_api/client.rb
+++ app/services/x_api/client.rb
@@ -2,6 +2,8 @@
 module XApi
   class Client
     BASE_URL = "https://api.twitter.com/2/users/by/username/"
+    # 🌟 リプライ取得用のエンドポイント
+    SEARCH_URL = "https://api.twitter.com/2/tweets/search/recent"
 
     def fetch_user_data(username)
       # 🌟 @が入っていたら取り除く（安全対策）
@@ -33,6 +35,44 @@ module XApi
         'created_at'      => data['created_at'] || Time.now.to_s,
         'default_profile' => false
       }
+    end
+
+    def fetch_replies(tweet_id)
+      conn = Faraday.new(url: SEARCH_URL) do |f|
+        f.request :url_encoded
+        f.adapter Faraday.default_adapter
+      end
+
+      response = conn.get do |req|
+        req.headers['Authorization'] = "Bearer #{ENV.fetch('X_BEARER_TOKEN')}"
+        # 🌟 ここがキモ！
+        # conversation_id: そのツイートに紐づく会話を全取得
+        # -is:retweet: リツイートは除外してスリムにする
+        req.params['query'] = "conversation_id:#{tweet_id} -is:retweet"
+        req.params['max_results'] = 100 # まずは100件（無料/廉価枠の限界値）
+        req.params['tweet.fields'] = 'author_id,created_at,text'
+        req.params['user.fields'] = 'verified,description'
+        req.params['expansions'] = 'author_id' # 投稿主の情報も一緒に連れてくる
+      end
+
+      return [] unless response.success?
+
+      body = JSON.parse(response.body)
+      return [] if body['data'].nil?
+
+      # 🌟 データの整形（Gemが食べやすい形にマージ）
+      # X API は「投稿データ」と「ユーザーデータ」が別々に届くので合体させます
+      users = body.dig('includes', 'users')&.index_by { |u| u['id'] } || {}
+      
+      body['data'].map do |tweet|
+        user = users[tweet['author_id']] || {}
+        {
+          'text' => tweet['text'],
+          'verified' => user['verified'] || false,
+          'description' => user['description'] || "",
+          'created_at' => tweet['created_at']
+        }
+      end
     end
   end
 end
