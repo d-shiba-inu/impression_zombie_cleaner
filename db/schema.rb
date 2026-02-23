aa31a0d7223ee047d4f4844cc598809b71bae1b4
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-23 18:10:37 +0900

    マイグレーションとバリデーションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_23_090143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "analyses", force: :cascade do |t|
+    t.string "url"
+    t.string "name"
+    t.string "screen_name"
+    t.text "text"
+    t.float "similarity_rate"
+    t.integer "score"
+    t.boolean "is_zombie"
+    t.boolean "verified"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["url"], name: "index_analyses_on_url"
+  end
 
 end
