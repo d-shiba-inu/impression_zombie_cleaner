afcfaf98f428517b32302c84c82d7fde458753d1
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-24 17:32:36 +0900

    認証バッジデータのマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_23_090143) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_24_081149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_23_090143) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "badge_type"
     t.index ["url"], name: "index_analyses_on_url"
   end
 
