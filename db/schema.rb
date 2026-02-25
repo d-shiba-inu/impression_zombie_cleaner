0048bea8b7f4dccbd48f2b57c95476a79be7c9ef
d-shiba-inu <daikiclassic8@gmail.com>
2026-02-25 19:56:04 +0900

    ツイート総数などのカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_24_081149) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_25_105016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_24_081149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "badge_type"
+    t.string "reply_lang"
+    t.string "profile_lang"
     t.index ["url"], name: "index_analyses_on_url"
   end
 
