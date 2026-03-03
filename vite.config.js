236eb586fb0285c9a5e12625828f917103686821
d-shiba-inu <daikiclassic8@gmail.com>
2026-03-03 12:49:53 +0900

    viteのビルド先の記述

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'; // ここを更新！
 export default defineConfig({
   plugins: [react()], // Reactプラグインを有効化
   root: 'src', 
+  build: {
+    // 🌟 ビルドしたファイルを Rails が読み込める public フォルダに出力
+    outDir: '../public',
+    emptyOutDir: true,
+  },
   server: {
     port: 3001,
   }
