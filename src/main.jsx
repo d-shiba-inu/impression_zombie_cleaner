cd9d22f20ac82a1eb1a1415b5ed7602a524b8e3e
d-shiba-inu <daikiclassic8@gmail.com>
2026-03-04 16:49:11 +0900

    main.jsxの変更

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // さっき作ったApp.js
+import { TopPage } from './pages/TopPage/index' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
